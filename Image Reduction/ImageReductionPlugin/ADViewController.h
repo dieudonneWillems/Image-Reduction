6e16162b531d42ee0bf784e2be820188c3a5373b
Don Willems <don_willems@me.com>
2013-06-03 23:43:06 +0200

    Added view controller to be extended by views provided in plugins.
    Including a definition of the view areas.

---
+++ Image Reduction/ImageReductionPlugin/ADViewController.h
@@ -0,0 +1,22 @@
+//
+//  ADViewController.h
+//  Image Reduction
+//
+//  Created by Don Willems on 03-06-13.
+//  Copyright (c) 2013 Lapsed Pacifist. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+typedef enum __ADViewArea {
+    ADNavigationSideViewArea,
+    ADMainViewArea,
+    ADInformationSideViewArea
+} ADViewArea;
+
+@interface ADViewController : NSViewController
+
+- (ADViewArea) preferredViewArea;
+
+@end
---
+++ Image Reduction/ImageReductionPlugin/ADViewController.m
@@ -0,0 +1,32 @@
+//
+//  ADViewController.m
+//  Image Reduction
+//
+//  Created by Don Willems on 03-06-13.
+//  Copyright (c) 2013 Lapsed Pacifist. All rights reserved.
+//
+
+#import "ADViewController.h"
+
+@interface ADViewController (private)
+
+@end
+
+@implementation ADViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (ADViewArea) preferredViewArea
+{
+    return ADMainViewArea;
+}
+
+@end
