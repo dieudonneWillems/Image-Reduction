55ad0bbd3f3cf2558ca3c29aa267996818445c51
Don Willems <don_willems@me.com>
2013-06-04 18:15:33 +0200

    Included License
    GPL-ed

--- Image Reduction/ImageReductionPlugin/ADDataObjectWrapper.h
+++ Image Reduction/ImageReductionPlugin/ADDataObjectWrapper.h
@@ -1,9 +1,25 @@
 //
 //  ADDataObjectWrapper.h
-//  Image Reduction
+//
+// 	This file is part of Image Reduction.
+//
+//    Image Reduction is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Image Reduction is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Image Reduction.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  Copyright (c) 2013 Dieudonné Willems. All rights reserved.
+//
 //
 //  Created by Don Willems on 29-05-13.
-//  Copyright (c) 2013 Lapsed Pacifist. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
--- Image Reduction/ImageReductionPluginTests/ImageReductionPluginTests.h
+++ Image Reduction/ImageReductionPluginTests/ImageReductionPluginTests.h
@@ -3,7 +3,7 @@
 //  ImageReductionPluginTests
 //
 //  Created by Don Willems on 27-05-13.
-//  Copyright (c) 2013 Lapsed Pacifist. All rights reserved.
+//
 //
 
 #import <SenTestingKit/SenTestingKit.h>
